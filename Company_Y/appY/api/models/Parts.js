386b82773c48e784ef41d3404e9c93c104ced48b
Hirenkumar Khant <hr266981@dal.ca>
2020-06-30 14:25:39 -0300

    made database connection

---
+++ Company_Y/appY/api/models/Parts.js
@@ -0,0 +1,30 @@
+/**
+ * Parts.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    id:{
+      type: 'number',
+      required: true,
+      allowNull: false,
+      columnName: 'partId',
+    },
+    partName:{
+      type: 'string',
+      required: true,
+      allowNull: false,
+    },
+    qoh:{
+      type: 'number',
+      required: true,
+      allowNull: false,
+    },
+  },
+  datastore: 'default',
+  tableName: 'parts',
+};
+
